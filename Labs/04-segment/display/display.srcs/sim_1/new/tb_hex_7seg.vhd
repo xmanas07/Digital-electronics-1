85ab4c79620f26d220cd0b5a745306a2a74f80c2
xmanas07 <tomas.manasek15@seznam.cz>
2021-03-09 18:10:32 +0100

    Finishing up

---
+++ Labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -0,0 +1,119 @@
+------------------------------------------------------------------------
+--
+-- Testbench for 7-segment display decoder.
+-- Nexys A7-50T, Vivado v2020.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------------------
+entity tb_hex_7seg is
+    -- Entity of testbench is always empty
+end entity tb_hex_7seg;
+
+------------------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------------------
+architecture testbench of tb_hex_7seg is
+
+    -- Local signals
+    signal s_hex  : std_logic_vector(4 - 1 downto 0);
+    signal s_seg  : std_logic_vector(7 - 1 downto 0);
+
+begin
+    -- Connecting testbench signals with decoder entity (Unit Under Test)
+    uut_hex_7seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => s_seg
+        );
+
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+
+        -- First test value
+        s_hex <= "0000"; wait for 50 ns;
+        assert (s_seg = "0000001")
+        report "Test failed for input combination: 0000" severity error;
+        
+         -- WRITE OTHER TEST CASES HERE
+         
+        s_hex <= "0001"; wait for 50 ns;
+        assert (s_seg = "1001111")
+        report "Test failed for input combination: 0001" severity error;
+        
+        s_hex <= "0010"; wait for 50 ns;
+        assert (s_seg = "0010010")
+        report "Test failed for input combination: 0010" severity error;
+
+        s_hex <= "0011"; wait for 50 ns;
+        assert (s_seg = "0000110")
+        report "Test failed for input combination: 0011" severity error;    
+        
+        s_hex <= "0100"; wait for 50 ns;
+        assert (s_seg = "1001100")
+        report "Test failed for input combination: 0100" severity error;
+
+        s_hex <= "0101"; wait for 50 ns;
+        assert (s_seg = "0100100")
+        report "Test failed for input combination: 0101" severity error;
+        
+        s_hex <= "0110"; wait for 50 ns;
+        assert (s_seg = "0100000")
+        report "Test failed for input combination: 0110" severity error;
+
+        s_hex <= "0111"; wait for 50 ns;
+        assert (s_seg = "0011111")
+        report "Test failed for input combination: 0111" severity error; 
+        
+        s_hex <= "1000"; wait for 50 ns;
+        assert (s_seg = "0000000")
+        report "Test failed for input combination: 1000" severity error;
+
+        s_hex <= "1001"; wait for 50 ns;
+        assert (s_seg = "0000100")
+        report "Test failed for input combination: 1001" severity error;
+        
+        s_hex <= "1010"; wait for 50 ns;
+        assert (s_seg = "0001000")
+        report "Test failed for input combination: 1010" severity error;
+
+        s_hex <= "1011"; wait for 50 ns;
+        assert (s_seg = "1100000")
+        report "Test failed for input combination: 1011" severity error;    
+        
+        s_hex <= "1100"; wait for 50 ns;
+        assert (s_seg = "0110001")
+        report "Test failed for input combination: 1100" severity error;
+
+        s_hex <= "1101"; wait for 50 ns;
+        assert (s_seg = "1000010")
+        report "Test failed for input combination: 1101" severity error;
+        
+        s_hex <= "1110"; wait for 50 ns;
+        assert (s_seg = "0110000")
+        report "Test failed for input combination: 1110" severity error;
+
+        s_hex <= "1111"; wait for 50 ns;
+        assert (s_seg = "0111000")
+        report "Test failed for input combination: 1111" severity error;                    
+       
+
+
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
